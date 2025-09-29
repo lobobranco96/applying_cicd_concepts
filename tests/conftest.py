61f60ccfb89422f410b0fc188e836d781f618d30
lobobranco96 <rntt2511@gmail.com>
2025-09-29 15:34:17 +0000

    feat spark fixture

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from pyspark.sql import SparkSession
+
+@pytest.fixture(scope="module")
+def spark():
+    spark = SparkSession.builder.master("local[*]").appName("Test").getOrCreate()
+    yield spark
+    spark.stop()
