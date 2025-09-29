aa6c70bb451ea933b0446fdbd835ef593df16950
lobobranco96 <rntt2511@gmail.com>
2025-09-29 12:24:09 +0000

    feat columns check

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,6 +1,8 @@
 from src.schema import get_csv_columns
 
-def test_columns_check():
+def test_expected_columns():
+    file_path = "../data/orders.csv"
+    
     expected_columns = ['order_id',
     'user_id',
     'product_id',
@@ -8,8 +10,8 @@ def test_columns_check():
     'total_price',
     'order_date',
     ]
-    file_path = "../data/orders.csv"
-    df_columns = orders_columns(file_path)
+
+    df_columns = get_csv_columns(file_path)
     assert df_columns == expected_columns (
         f"Colunas diferentes!\nEsperado: {expected_columns}\nEncontrado: {df_columns}"
     )
