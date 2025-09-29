dde3ed75c885c891a0ef501362fdbc43fff784ff
lobobranco96 <rntt2511@gmail.com>
2025-09-29 12:12:47 +0000

    feat columns check

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
-def soma(a, b):
-    return a + b
+import pandas as pd
+
+def orders_columns(orders_file_path):
+  return list(pd.read_csv(orders_file_path).columns)
 
 if __name__ == "__main__":
-    print("Resultado:", soma(2, 3))
+  file_path = "/content/drive/MyDrive/projetos/ecommerce-pipeline/include/2025-09-22/orders.csv"
+  print("Resultado:", orders_columns(file_path))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,15 @@
-from src.app import soma
+from src.app import orders_columns
 
-def test_soma():
-    assert soma(2, 3) == 5
+def test_columns_check():
+    expected_columns = ['order_id',
+    'user_id',
+    'product_id',
+    'quantity',
+    'total_price',
+    'order_date',
+    ]
+    file_path = "/content/drive/MyDrive/projetos/ecommerce-pipeline/include/2025-09-22/orders.csv"
+    df_columns = orders_columns(file_path)
+    assert df_columns == expected_columns (
+        f"Colunas diferentes!\nEsperado: {expected_columns}\nEncontrado: {df_columns}"
+    )
